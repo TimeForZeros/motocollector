3a15b453f87ed18cdde9289ec62086028dc1764d
Victor Gonzalez <dazedbutamused@gmail.com>
2019-09-10 16:00:35 -0700

    updated models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Moto(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField(max_length=4)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'moto_id': self.id})
+
+class Maintenance(models.Model):
+    date = models.DateField('Maintenance date')
+    maintenance = models.Charfield(
+        max_length=1,
+        choices=MAINT,
+        default=MAINT[0][0]
+        )
+moto = models.ForeignKey(Moto, on_delete=models.CASCADE)
+
+def __str__(self):
+    return f'{self.get_meal_display()} on {self.date}'
