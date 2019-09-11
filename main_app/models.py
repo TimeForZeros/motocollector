b0438d78887d97904dcb32a225a2a0d787dcaa33
Victor Gonzalez <dazedbutamused@gmail.com>
2019-09-11 14:05:02 -0700

    aws functionality

--- main_app/models.py
+++ main_app/models.py
@@ -36,3 +36,9 @@ class Maintenance(models.Model):
     class Meta:
         ordering = ['-date']
 
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    moto = models.ForeignKey(Moto, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'Photo for moto_id: {self.moto_id} @{self.url}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Moto
+
+import uuid
+import boto3
+
+from .models import Moto, Photo
 from .forms import MaintenanceForm
+
+S3_BASE_URL =  'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'motocollector'
 
 class MotoCreate(CreateView):
   model = Moto
@@ -39,3 +46,9 @@ def motos_detail(request, moto_id):
       new_maintenance.moto_id = moto_id
       new_maintenance.save()
     return redirect('detail', moto_id=moto_id)
+
+  def add_photo(request, moto_id):
+    photo_file = request.FILES.get('photo-file', NONE)
+    if photo_file:
+      s3 = boto3.client('s3')
+      key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
